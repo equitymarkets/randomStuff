4ca00236efaee59cf15ae9f4d751d6ca5152efe7
Derek <49753517+equitymarkets@users.noreply.github.com>
2023-02-22 15:48:31 -0500

    Create fizzbuzz.py

---
+++ fizzbuzz.py
@@ -0,0 +1,9 @@
+for number in range(0, 100):
+    if number % 15 == 0:
+        print('fizzbuzz')
+    elif number % 3 == 0:
+        print('fizz')
+    elif number % 5 == 0:\
+        print('buzz')
+    else:
+        print(number)
